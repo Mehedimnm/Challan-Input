8fec774504f91d34c482ac40ccb65a2d7fc679f4
Md. Mehedi Hasan <141256178+Mehedimnm@users.noreply.github.com>
2026-01-03 19:10:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -235,8 +235,8 @@ def process_data(user_input, client_ua):
 
         # 3. Logic Setup
         cbo_logic = '1'
-        if user_input.startswith('8000'): cbo_logic = '4'
-        elif user_input.startswith('10'): cbo_logic = '2'
+        if user_input.startswith('8'): cbo_logic = '4'
+        elif user_input.startswith('2'): cbo_logic = '2'
 
         ctrl_url = f"{base_url}/production/requires/bundle_wise_cutting_delevar_to_input_controller.php"
         headers_ajax = headers_common.copy()
@@ -390,6 +390,7 @@ def process():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
+
 
 
 
